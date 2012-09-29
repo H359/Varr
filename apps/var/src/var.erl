2d94791560ea7745485466a41d3ce8067799b9cf
Crazy_Owl <stalker401@gmail.com>
2012-09-29 18:33:14 +0400

    added function for getting config values

--- apps/var/src/var.erl
+++ apps/var/src/var.erl
@@ -6,3 +6,9 @@ start() ->
     ok = application:start(cowboy),
     ok = application:start(eredis), %dunno if this is actually needed
     ok = application:start(var_app).
+
+get_config_value(Key, Default) ->
+    case application:get_env(var, Key) of
+        {ok, Value} -> Value;
+        _ -> Default
+    end.
