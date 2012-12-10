17d5467ae3fb15cfd72c2cafae4af5c940b0b513
Crazy_Owl <stalker401@gmail.com>
2012-12-10 21:23:25 +0400

    change in uuid_server starting mechanism

--- apps/varr/src/uuid_server.erl
+++ apps/varr/src/uuid_server.erl
@@ -7,7 +7,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 start() ->
-    gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+    gen_server:start(?MODULE, [], []).
 
 gen() ->
     gen_server:call(?MODULE, {gen}).
